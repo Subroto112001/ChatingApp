a26c42dd2aed201e5f1d45f44f314312507fd249
Subroto112001 <subrotokumarbarman@gmail.com>
2025-03-22 11:01:39 +0600

    some line added

--- src/Component/HomePageComponent/Slidebar.jsx
+++ src/Component/HomePageComponent/Slidebar.jsx
@@ -1,52 +1,89 @@
-import React from 'react'
-import ProfileImage from "../../assets/ProfilePic.jpg"
-import { IoCloudUpload, IoNotifications } from 'react-icons/io5';
-import { FaHome } from 'react-icons/fa';
-import { FiMessageSquare } from 'react-icons/fi';
-import { FaGear } from 'react-icons/fa6';
-import { ImExit } from 'react-icons/im';
- import { useState } from "react";
-import { useLocation, useNavigate } from 'react-router';
+import React, { useEffect } from "react";
+import ProfileImage from "../../assets/ProfilePic.jpg";
+import { IoCloudUpload, IoNotifications } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
+import { FiMessageSquare } from "react-icons/fi";
+import { FaGear } from "react-icons/fa6";
+import { ImExit } from "react-icons/im";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router";
 const Slidebar = () => {
-
   const pagenavigate = useNavigate();
-const location = useLocation()
- 
-    const elimentofSlidebar = [
-      {
-        id: 1,
-        path: "/",
-        icon: <FaHome />,
-      },
-      {
-        id: 2,
-        path: "/message",
-        icon: <FiMessageSquare />,
-      },
-      {
-        id: 3,
-        path: "/notification",
-        icon: <IoNotifications />,
-      },
-      {
-        id: 4,
-        path: "/settings",
-        icon: <FaGear />,
-      },
-      {
-        id: 5,
-        path: "/login",
-        icon: <ImExit />,
-      },
-    ];
+  const location = useLocation();
+
+  const elimentofSlidebar = [
+    {
+      id: 1,
+      path: "/",
+      icon: <FaHome />,
+    },
+    {
+      id: 2,
+      path: "/message",
+      icon: <FiMessageSquare />,
+    },
+    {
+      id: 3,
+      path: "/notification",
+      icon: <IoNotifications />,
+    },
+    {
+      id: 4,
+      path: "/settings",
+      icon: <FaGear />,
+    },
+    {
+      id: 5,
+      path: "/login",
+      icon: <ImExit />,
+    },
+  ];
 
   const handleNavigatePage = (path = "/") => {
     console.log(path);
-    pagenavigate(path)
-   
+    pagenavigate(path);
   };
- 
- 
+
+  /*
+   * todo : handleUploadImage
+   */
+  const handleUploadImage = () => {
+    cloudinary.openUploadWidget(
+      {
+        cloudName: "df8qz4g9h",
+        uploadPreset: "ChatAppFile",
+        sources: [
+          "local",
+          "url",
+          "camera",
+          "dropbox",
+          "unsplash",
+          "google_drive",
+          "shutterstock",
+          "image_search",
+          "gettyimages",
+          "istock",
+        ],
+        googleApiKey: "AIzaSyAykP0egZO9VbeFAJ8hBJE5td7ho2gcOXY",
+        searchBySites: ["all", "cloudinary.com"],
+        searchByRights: true,
+      },
+      (err, result) => {
+        if (err) {
+          throw new Error("Failed to upload image ");
+        }
+
+        console.log(result.info.sucess_url);
+      }
+    );
+  };
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://upload-widget.cloudinary.com/latest/global/all.js`;
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+  console.log(window.cloudinary);
 
   return (
     <>
@@ -59,7 +96,10 @@ const location = useLocation()
               className="w-full h-full rounded-full object-cover "
             />
           </picture>
-          <span className="absolute hidden left-[50%] top-[50%] text-white text-2xl -translate-[50%] group-hover:block">
+          <span
+            onClick={handleUploadImage}
+            className="absolute hidden left-[50%] top-[50%] text-white text-2xl -translate-[50%] group-hover:block"
+          >
             <IoCloudUpload />
           </span>
         </div>
@@ -68,7 +108,7 @@ const location = useLocation()
             {elimentofSlidebar?.map((item, index) =>
               elimentofSlidebar.length - 1 == index ? (
                 <div
-                  className="text-[28px] text-white mt-20 cursor-pointer"
+                  className="text-[29px] text-white mt-20 cursor-pointer"
                   key={item.id}
                   onClick={() => handleNavigatePage(item.path)}
                 >
@@ -103,6 +143,6 @@ const location = useLocation()
       </div>
     </>
   );
-}
+};
 
-export default Slidebar
+export default Slidebar;
