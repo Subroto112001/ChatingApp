8b06ccf5978640920a065432fc2333741eaf3430
Subroto112001 <subrotokumarbarman@gmail.com>
2025-05-07 21:36:08 +0600

    update

--- src/Component/Eliment/HomePageEliment/GroupList.jsx
+++ src/Component/Eliment/HomePageEliment/GroupList.jsx
@@ -3,6 +3,9 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import Profilegroup from "../../../assets/FriendGroup.jpg";
 import { getDatabase, onValue, ref, set } from "firebase/database";
 import LoadingSkeliton from "../Skeliton/LoadingSkeliton";
+import Modal from "react-modal";
+
+
 const GroupList = ({
   BtnStyle,
   CardEliment,
@@ -18,6 +21,34 @@ const GroupList = ({
   const [userlist, setUserlist] = useState([]);
   const [loading, setLoading] = useState(false);
   const db = getDatabase();
+const [modalIsOpen, setIsOpen] = useState(false);
+
+  // react modal custom style
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+  // react modal custom style
+/**
+ * 
+ * This Two function will work for modal opening and closing
+ * 
+ * */ 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+    function closeModal() {
+      setIsOpen(false);
+    }
+
 
   useEffect(() => {
     setLoading(true);
@@ -32,6 +63,7 @@ const GroupList = ({
         setUserlist(userBlanklist);
         setLoading(false);
       });
+
     };
     fetchdata();
   }, []);
@@ -53,7 +85,12 @@ const GroupList = ({
                 {Totalnumber}
               </span>
             </h2>
-            <BsThreeDotsVertical className=" text-blue" />
+            <button
+              className="p-2 bg-blue text-white font-bold text-[14px] cursor-pointer rounded "
+              onClick={openModal}
+            >
+              Create Group
+            </button>
           </div>
           <div className={BoxStyle}>
             {[...new Array(Totalnumber)].map((_, index) => (
@@ -85,6 +122,66 @@ const GroupList = ({
           </div>
         </div>
       </div>
+
+      {/* modal code  */}
+
+      <div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+        >
+          <div>
+            <button
+              onClick={closeModal}
+              className="text-red-600 bg-blue p-2 rounded font-bold text-[12px] cursor-pointer"
+            >
+              Close
+            </button>
+
+            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+              <div class="mb-6">
+                <label
+                  for="success"
+                  class="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
+                >
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  id="success"
+                  className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                  placeholder="Success input"
+                />
+                <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+                  <span className="font-medium">Well done!</span> Some success
+                  message.
+                </p>
+              </div>
+              <div className="mb-6">
+                <label
+                  for="success"
+                  className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
+                >
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  id="success"
+                  className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                  placeholder="Success input"
+                />
+                <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+                  <span className="font-medium">Well done!</span> Some success
+                  message.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </div>
+
+      {/* modal code  */}
     </>
   );
 };
